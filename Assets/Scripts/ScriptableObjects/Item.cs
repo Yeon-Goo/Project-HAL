e2944ba9cdb3b84c44cc72d6b7e4beaa81fb0122
pwdab <soundno07@naver.com>
2024-03-17 23:43:30 +0900

    Revert "24.3.17"
    This reverts commit 03b7f33dcbd472fde3e8570c5423aa640430300a.

--- Assets/Scripts/ScriptableObjects/Item.cs
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -8,13 +8,13 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     // Item의 이름
-    public string objectName;
+    private string objectName;
     // Item의 sprite
     private Sprite sprite;
     // Item의 수량 (default : 1)
-    public int quantity;
+    private int quantity;
     // Item의 stack 여부 (default : false)
-    public bool stackable;
+    private bool stackable;
 
     // ** 여기 private enum으로 하면 왜 GetItemType()에서 오류가 나는지 모르겠음 **
     public enum ItemTypeEnum
@@ -23,7 +23,7 @@ public class Item : ScriptableObject
         HEALTH
     }
     // Item의 타입
-    public ItemTypeEnum itemType;
+    private ItemTypeEnum itemType;
 
     public string GetName()
     {
