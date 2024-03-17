2b747763afe75bbb26fb9c1f1222fca004ae7a98
pwdab <soundno07@naver.com>
2024-03-18 01:04:50 +0900

    Revert "Merge pull request #7 from Yeon-Goo/main"
    This reverts commit bc8ef2210f0ab2d6ad6d3f1e090f31c35e7c1472, reversing
    changes made to d958bc550d22a517fe20edecc543faa8f54e6f4c.

--- Assets/Scripts/MonoBehaviour/PlayerEntity.cs
+++ Assets/Scripts/MonoBehaviour/PlayerEntity.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class PlayerEntity : Entity
 {
     private static string pickable_Objects = "Pickable_Objects";
+    public HPBar hpbar_Prefab;
+    private HPBar hpBar;
 
     void Start()
     {
-        // Need to fill something...
+        cur_HP.value = init_HP;
+        hpBar = Instantiate(hpbar_Prefab);
+        hpBar.SetPlayer(this);
     }
 
 
@@ -20,9 +25,37 @@ public class PlayerEntity : Entity
 
             if(hitObject != null)
             {
+                bool should_disappear = false;
+
                 print("Hit: " + hitObject.GetName());
-                collision.gameObject.SetActive(false);
+                switch (hitObject.GetItemType())
+                {
+                    case Item.ItemTypeEnum.COIN:
+                        should_disappear = true;
+                        break;
+                    case Item.ItemTypeEnum.HEALTH:
+                        should_disappear = AdjustHP(hitObject.GetQuantity());
+                        break;
+                }
+
+                if (should_disappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
             }
         }
+    }
+
+    private bool AdjustHP(int amount)
+    {
+        if (cur_HP.value < max_HP)
+        {
+            cur_HP.value += amount;
+            print("Adjusted HP by : " + amount + ". New value : " + cur_HP.value);
+
+            return true;
+        }
+
+        return true;
     }
 }
