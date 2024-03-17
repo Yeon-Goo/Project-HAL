6965e7e2ff7e2d9fa1e8f852228aab0c9fc363fa
harneng99 <144768337+harneng99@users.noreply.github.com>
2024-03-17 21:48:26 +0900

    맵 확장 및 코드 정리
    맵 확장 및 코드 정리

--- Assets/Scripts/MonoBehaviour/Green_slimeEntityMovementControler.cs
+++ Assets/Scripts/MonoBehaviour/Green_slimeEntityMovementControler.cs
@@ -9,18 +9,17 @@ public class MonsterTracking : MonoBehaviour
     public float attack_range = 2f;
     public float attack_speed = 5f;
     public float attack_charge = 2f;
-    public float attack_time = 3f;
-    public float attack_delay = 5f;
-    private bool isCharging = false;
-    private bool isDelay = false;
-    private bool isTracking = false;
+    public float attack_time = 1f;
+    public float attack_delay = 2f;
+    private int monster_state = 0;
     private float tempTime = 0f;
+    private Vector2 player_pos;
 
 
     private Animator animator;
     private static string animationState = "AnimationState";
 
-    enum AnimationStateEnum
+    enum StateEnum
     {
         idle = 0,
         move = 1,
@@ -29,57 +28,68 @@ public class MonsterTracking : MonoBehaviour
         delay = 4
     }
 
-#if DEBUG
-    public float debug_velocity;
-    public Vector2 debug_vector;
-    public float debug_vector_x;
-    public bool debug_is_moveable;
-    public int debug_animationstate;
-#endif
-
-
-    void Update()
+    void FixedUpdate()
     {
         float distance = Vector2.Distance(transform.position, player.position);
-        if(!isTracking && distance < range)
+        if(monster_state == (int)StateEnum.idle) // 평상시
         {
-            isTracking = true;
-        }
-        if (distance <= tracking_range && distance > attack_range && !isCharging && !isDelay && isTracking) // 추격 중
-        {
-            //transform.LookAt(player);
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        if (distance <= attack_range && !isCharging && !isDelay) // 공격 대기
-        {
-            //transform.LookAt(player);
-            isCharging = true;
-            tempTime = Time.time;
-        }
-        if (isCharging && Time.time - tempTime > attack_charge && !isDelay) // 공격
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, attack_speed * Time.deltaTime);
-            if(Time.time - tempTime > attack_time)
+            if(distance < range) // 거리가 가까워지면 추격상태로 전환
             {
-                isDelay = true;
+                monster_state = (int)StateEnum.move;
             }
         }
-        if (isCharging && Time.time - tempTime <= attack_charge && !isDelay) // 공격 선딜
+
+        if (monster_state == (int)StateEnum.move) // 추격 중
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        }
-        if(isDelay) // 공격 후딜
-        {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            if(Time.time - tempTime > attack_delay)
+            if(distance <= tracking_range && distance > attack_range) // 공격 사거리가 안 될 시
             {
-                isDelay = false;
-                isCharging = false;
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            if (distance <= attack_range) // 공격 사거리가 됐을 경우 차지상태로 전환
+            {
+                player_pos = new Vector2(player.position.x, player.position.y);
+                monster_state = (int)StateEnum.charge;
+                tempTime = Time.time;
+            }
+            if(distance > tracking_range) // 시야 사거리가 멀어졌을 경우 평상시로 전환
+            {
+                monster_state = (int)StateEnum.idle;
             }
         }
-        if(isTracking && !isDelay && !isCharging && distance > tracking_range)
+
+        if (monster_state == (int)StateEnum.charge) // 차지 중
         {
-            isTracking = false;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero; // 대기
+            if(Time.time - tempTime >= attack_charge) // 차지시간이 지났으면 공격상태로 전환
+            {
+                tempTime = Time.time;
+                monster_state = (int)StateEnum.attack;
+            }
+        }
+        
+        if (monster_state == (int)StateEnum.attack) // 공격
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player_pos, attack_speed * Time.deltaTime);
+            if(Time.time - tempTime > attack_time) // 공격이 끝나고 딜레이로 전환
+            {
+                monster_state = (int)StateEnum.delay;
+            }
+        }
+
+        if(monster_state == (int)StateEnum.delay) // 딜레이 중일 때
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            if(Time.time - tempTime > attack_delay) // 딜레이 시간이 끝나고
+            {
+                if(distance < tracking_range)   // 돌진 후 가까우면 추격상태로 전환
+                {
+                    monster_state = (int)StateEnum.move;
+                }
+                else  // 돌진 후 멀면 평상시로 전환
+                {
+                    monster_state = (int)StateEnum.idle;
+                }
+            }
         }
     }
 }
