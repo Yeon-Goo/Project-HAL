a3676190bcda36f4ccd8d07baebdbb4e26221a6c
pwdab <soundno07@naver.com>
2024-03-18 01:08:41 +0900

    Revert "Revert "Merge pull request #7 from Yeon-Goo/main""
    This reverts commit 2b747763afe75bbb26fb9c1f1222fca004ae7a98.

--- Assets/Scripts/ScriptableObjects/HP.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "HP")]
-public class HP : ScriptableObject
-{
-    public float value;
-}
