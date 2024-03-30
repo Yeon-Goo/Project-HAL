cf31484ad76eda430d5fd38aad86bf26af9e9745
pwdab <soundno07@naver.com>
2024-03-30 21:31:24 +0900

    git 한글 주석 test2

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -7,15 +7,26 @@ public class GameManager : MonoBehaviour
     // 게임 매니저는 오직 하나여야 한다
     public static GameManager sharedInstance = null;
 
+    private void Awake()
+    {
+        if (sharedInstance != null && sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            sharedInstance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupScene();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetupScene()
     {
-        
+        //
     }
 }
