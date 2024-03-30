11c619dea954a3b344ed48fde7e055f15031608f
pwdab <soundno07@naver.com>
2024-03-30 21:23:57 +0900

    git 한글 주석 test

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 게임 매니저는 오직 하나여야 한다
+    public static GameManager sharedInstance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
