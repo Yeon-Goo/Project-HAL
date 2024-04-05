f37e51b86abd59302f11d4186ca0b2eb0893483a
최연구 <augustinodot@naver.com>
2024-04-05 12:58:27 +0900

    fuck

--- Assets/Scripts/MonoBehaviour/Weapons/ArrowObject.cs
+++ Assets/Scripts/MonoBehaviour/Weapons/ArrowObject.cs
@@ -1,36 +1,70 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class ArrowObject : MonoBehaviour
 {
-    public float speed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 startPosition;
+    // 화살 설정
+    private int dmg;
+    private float armor_de, speed;
+    private Vector3 shootvector = Vector3.zero;
 
-    void Start()
+    // 화살 객체 풀링
+    private IObjectPool<ArrowObject> _ManagedPool;
+    bool is_released = false;
+
+    // 화살이 enemy와 충돌할 경우 호출되는 메서드
+    private void OnTriggerEnter2D(Collider2D coll)
     {
-        rb = this.GetComponent<Rigidbody2D>();
-        startPosition = rb.position;
-        rb.velocity = transform.right * speed;
+        if (coll.gameObject.tag == "enemy")
+        {
+            //coll.gameObject.GetComponent<EnemyObject>().Damaged(dmg, armor_de);
+            DestroyArrow();
+        }
+    }
+
+    // 화살이 발사되는 순간에 호출되는 메서드
+    public void Shoot(Vector3 dir)
+    {
+        is_released = false;
+        shootvector = dir;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90); // 화살의 회전 각도 조정
+        //StopAllCoroutines();
+        //StartCoroutine(DestroyArrowAfterTime(4.0f)); // 일정 시간 후 자동 파괴
+    }
+
+    private IEnumerator DestroyArrowAfterTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        DestroyArrow();
     }
 
     void Update()
     {
-        // destroy when distance > 10
-        if (Vector2.Distance(startPosition, rb.position) > 10)
-        {
-            Destroy(gameObject);
-        }
+        // 화살이 회전한 축의 y 방향으로 시간*속도 만큼 이동함
+        transform.Translate(new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime * speed);
     }
 
-    void OnTriggerEnter2D(Collider2D hitInfo)
+    public void SetArrowData(int damage, float decrease, float shootspeed)
     {
-        if (hitInfo.gameObject.CompareTag("enemy"))
+        dmg = damage;
+        armor_de = decrease;
+        speed = shootspeed;
+    }
+
+    public void SetManagedPool(IObjectPool<ArrowObject> pool)
+    {
+        _ManagedPool = pool;
+    }
+
+    public void DestroyArrow()
+    {
+        if (!is_released)
         {
-            //need to fix to destory -> damage
-            Destroy(hitInfo.gameObject);
-            Destroy(gameObject);
+            _ManagedPool.Release(this);
+            is_released = true;
         }
     }
-} 
+}
