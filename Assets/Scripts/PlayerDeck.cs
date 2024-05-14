eb14ba683bd7856716bff46e8611d20717839ce9
최연구 <augustinodot@naver.com>
2024-05-14 22:21:10 +0900

    Dash Update
    FUK

--- Assets/Scripts/PlayerDeck.cs
+++ Assets/Scripts/PlayerDeck.cs
@@ -85,7 +85,6 @@ public class PlayerDeck : MonoBehaviour
 
         playerEntity.is_looking_right = (playerEntity.GetMousePos().x > playerEntity.GetPos().x) ? true : false;
 
-        Debug.Log("Base Attack");
         if(weapon.GetComponent<Weapon>().BaseAttackAble())
         {
             weapon.GetComponent<Weapon>().BaseAttack();
