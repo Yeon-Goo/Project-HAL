fed3357ffef2151d58fe177e4e501e590f85c774
honghyungi1 <153365523+honghyungi1@users.noreply.github.com>
2024-06-03 23:50:43 +0900

    포탈 및 간단한 stage1 / 보스맵

---
+++ Assets/Scripts/Un-combat/Boss_Portal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boss_Portal : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+             if (SceneManager.GetActiveScene().name == "Stage0")
+             {
+                Vector3 targetposition = new Vector3(4,-24,0);
+                other.transform.position = targetposition;
+
+             }
+        }
+    }
+        
+    
+}
