cdafd9e3970507c1a08556e80d39309b12ba68fc
harneng99 <144768337+harneng99@users.noreply.github.com>
2024-06-09 16:55:34 +0900

    24.06.09
    일반 몬스터 에셋 적용 및 공격 알고리즘 일부 수정

--- Assets/Scripts/monster/Green_slimeEntityMovementControler.cs
+++ Assets/Scripts/monster/Green_slimeEntityMovementControler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Spine.Unity;
 
 public class MonsterTracking : MonoBehaviour
 {
@@ -8,137 +9,124 @@ public class MonsterTracking : MonoBehaviour
     public float tracking_range = 7f;
     public float attack_range = 2f;
     public float attack_speed = 5f;
-    public float attack_charge = 2f;
-    public float attack_time = 1f;
-    public float attack_delay = 2f;
+    public float attack_time = 0.7f;
+    public float idle_delay = 1f; // Idle 상태 유지 시간
     private int monster_state = 0;
     private float tempTime = 0f;
     private Vector2 initial_pos;
     private Vector2 wanderDirection;
     private Vector2 move_direction;
     private Vector3 target_pos;
+    private bool after_Attack = false;
 
-
-    private Animator animator;
+    private Rigidbody2D rb;
+    private SkeletonAnimation skeletonAnimation;
     private static string animationState = "AnimationState";
 
     enum StateEnum
     {
-        idle = 0,
-        move = 1,
-        charge = 2,
-        attack = 3,
-        delay = 4,
-        idle_move = 5
+        Idle = 0,
+        Walk = 1,
+        Attack = 2
     }
+
     void Start()
-    {        
+    {
         GameObject playerObject = GameObject.Find("PlayerObject");
         if (playerObject != null)
         {
             player = playerObject.transform;
         }
         initial_pos = transform.position;
-        animator = GetComponent<Animator>();
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody2D를 가져옴
     }
+
     void FixedUpdate()
     {
         float distance = Vector2.Distance(transform.position, player.position);
-        if(monster_state == (int)StateEnum.idle) // 평상시
+
+        if (monster_state == (int)StateEnum.Idle) // 평상시
         {
-            if(distance < range) // 거리가 가까워지면 추격상태로 전환
+            if(after_Attack == false)
             {
-                monster_state = (int)StateEnum.move;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                animator.SetInteger(animationState, monster_state);
+                if (distance < range) // 거리가 가까워지면 추격상태로 전환
+                {
+                    monster_state = (int)StateEnum.Walk;
+                    SetAnimation("Walk", true);
+                }
+                else if (Time.time - tempTime > 1f) // 평상시 상태가 1초 이상 지속시 탐색상태로 전환
+                {
+                    wanderDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                    Vector2 From_Current_To_Initial = (initial_pos - (Vector2)transform.position).normalized;
+                    move_direction = (From_Current_To_Initial + wanderDirection).normalized;
+                    target_pos = transform.position + (Vector3)move_direction * 10;
+                    tempTime = Time.time;
+                    monster_state = (int)StateEnum.Walk;
+                    SetAnimation("Walk", true);
+                }
             }
-            else if(Time.time - tempTime > 1f) // 평상시 상태가 1초 이상 지속시 탐색상태로 전환
+            else if (Time.time - tempTime > 0.5f)
             {
-                wanderDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
-                Vector2 From_Current_To_Initial = (initial_pos - (Vector2)transform.position).normalized;
-                move_direction = (From_Current_To_Initial + wanderDirection).normalized;
-                target_pos = transform.position + (Vector3)move_direction * 10;
-                tempTime = Time.time;
-                monster_state = (int)StateEnum.idle_move;
+                after_Attack = false;
             }
         }
 
-        if (monster_state == (int)StateEnum.move) // 추격 중
+        if (monster_state == (int)StateEnum.Walk) // 추격 중
         {
-            if(distance <= tracking_range && distance > attack_range) // 공격 사거리가 안 될 시
+            if (distance <= tracking_range && distance > attack_range) // 공격 사거리가 안 될 시
             {
                 transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                FlipSprite(); // 스프라이트의 방향을 지속적으로 업데이트
             }
-            if (distance <= attack_range) // 공격 사거리가 됐을 경우 차지상태로 전환
+            if (distance <= attack_range) // 공격 사거리가 됐을 경우 공격상태로 전환
             {
                 move_direction = (player.position - transform.position).normalized;
-                target_pos = transform.position + (Vector3)move_direction * 3;
-                monster_state = (int)StateEnum.charge;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                animator.SetInteger(animationState, monster_state);
+                rb.velocity = Vector2.zero; // 기존의 속도를 초기화
+                rb.AddForce(move_direction * attack_speed, ForceMode2D.Impulse); // 플레이어 방향으로 힘을 가함
+                monster_state = (int)StateEnum.Attack;
+                SetAnimation("Attack", false);
                 tempTime = Time.time;
             }
-            if(distance > tracking_range) // 시야 사거리가 멀어졌을 경우 평상시로 전환
+            if (distance > tracking_range) // 시야 사거리가 멀어졌을 경우 평상시로 전환
             {
-                monster_state = (int)StateEnum.idle;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                animator.SetInteger(animationState, monster_state);
+                monster_state = (int)StateEnum.Idle;
+                SetAnimation("Idle", true);
                 tempTime = Time.time;
             }
         }
 
-        if (monster_state == (int)StateEnum.charge) // 차지 중
+        if (monster_state == (int)StateEnum.Attack) // 공격
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero; // 대기
-            if(Time.time - tempTime >= attack_charge) // 차지시간이 지났으면 공격상태로 전환
+            if (Time.time - tempTime > attack_time) // 공격 애니메이션이 끝나고
             {
+                rb.velocity = Vector2.zero;
+                monster_state = (int)StateEnum.Idle; // Idle 상태로 전환하여 1초 유지
+                SetAnimation("Idle", true);
+                after_Attack = true;
                 tempTime = Time.time;
-                monster_state = (int)StateEnum.attack;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                animator.SetInteger(animationState, monster_state);
             }
         }
-        
-        if (monster_state == (int)StateEnum.attack) // 공격
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target_pos, attack_speed * Time.deltaTime);
-            if(Time.time - tempTime > attack_time) // 공격이 끝나고 딜레이로 전환
-            {
-                monster_state = (int)StateEnum.delay;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                animator.SetInteger(animationState, (int)StateEnum.delay);
-            }
-        }
+    }
 
-        if(monster_state == (int)StateEnum.delay) // 딜레이 중일 때
+    private void FlipSprite()
+    {
+        // 플레이어의 위치와 몬스터의 위치를 비교하여 스케일을 조정합니다.
+        if (player != null)
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            if(Time.time - tempTime > attack_delay) // 딜레이 시간이 끝나고
+            if (transform.position.x > player.position.x)
             {
-                if(distance < tracking_range)   // 돌진 후 가까우면 추격상태로 전환
-                {
-                    monster_state = (int)StateEnum.move;
-                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                    animator.SetInteger(animationState, monster_state);
-                }
-                else  // 돌진 후 멀면 평상시로 전환
-                {
-                    monster_state = (int)StateEnum.idle;
-                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                    animator.SetInteger(animationState, monster_state);
-                    tempTime = Time.time;
-                }
+                transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f); // 오른쪽에 있을 경우 y축 대칭
+            }
+            else
+            {
+                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // 왼쪽에 있을 경우 정상
             }
         }
+    }
 
-        if(monster_state == (int)StateEnum.idle_move) // 탐색 상태일 때
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target_pos, speed * Time.deltaTime);
-            if(Time.time - tempTime > 1f)
-            {
-                tempTime=Time.time;
-                monster_state = (int)StateEnum.idle;
-            }
-        }
+    private void SetAnimation(string animationName, bool loop)
+    {
+        skeletonAnimation.state.SetAnimation(0, animationName, loop);
     }
 }
