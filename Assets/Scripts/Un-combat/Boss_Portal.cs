7d123787e21ae3d5982e1f9f6d5fe517a1175bbc
pwdab <soundno07@naver.com>
2024-06-12 21:38:57 +0900

    Revert "포탈 수정"
    This reverts commit fe25ea4deff6f93a21213f1dbd76ff9e3c3b6462.

--- Assets/Scripts/Un-combat/Boss_Portal.cs
+++ Assets/Scripts/Un-combat/Boss_Portal.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Boss_Portal : MonoBehaviour
 {
-    public GameObject portal;
-    private GameObject boss;
-     void Update()
-    {
-        boss = GameObject.Find("king_slime");
-        if (boss == null)
-        {
-                portal.SetActive(true);  
-            
-        }
-    }
-
 
     void OnCollisionEnter2D(Collision2D other)
     {
